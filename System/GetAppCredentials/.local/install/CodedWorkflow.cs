442463021291b07a8ac7cf7d0060717383fee538
Catarina Valente <catarina.mfv@gmail.com>
2024-02-28 12:57:37 +0000

    Add System Reusable Modules

---
+++ System/GetAppCredentials/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace GetCredential
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
---
+++ System/INIT_GetOrchestratorAssets/.local/install/CodedWorkflow.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Orchestrator.Client.Models;
+using UiPath.Testing;
+using UiPath.Testing.Activities.TestData;
+using UiPath.Testing.Activities.TestDataQueues.Enums;
+using UiPath.Testing.Enums;
+using UiPath.CodedWorkflows.DescriptorIntegration;
+
+namespace INIT_GetOrchestratorAssets
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService)};
+        }
+
+        protected UiPath.Core.Activities.API.ISystemService system { get => serviceContainer.Resolve<UiPath.Core.Activities.API.ISystemService>(); }
+
+        protected UiPath.Testing.API.ITestingService testing { get => serviceContainer.Resolve<UiPath.Testing.API.ITestingService>(); }
+    }
+}
+
+namespace INIT_GetOrchestratorAssets.ObjectRepository
+{
+    public static class Descriptors
+    {
+    }
+}
+
+namespace INIT_GetOrchestratorAssets._Implementation
+{
+    internal class ScreenDescriptorDefinition : IScreenDescriptorDefinition
+    {
+        public IScreenDescriptor Screen { get; set; }
+
+        public string Reference { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+
+    internal class ElementDescriptorDefinition : IElementDescriptorDefinition
+    {
+        public IScreenDescriptor Screen { get; set; }
+
+        public string Reference { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public IElementDescriptor ParentElement { get; set; }
+
+        public IElementDescriptor Element { get; set; }
+    }
+}
